d3d63837a3b8d0f5fa6c5a0a279d75a319d4f24e
DESKTOP-UCE6VO1\Asus-MM <yordananastasov@gmail.com>
2023-08-17 16:45:19 +0300

    Some changes in settings and urls

--- tour_guide_bulgaria/tour_guide_bulgaria/urls.py
+++ tour_guide_bulgaria/tour_guide_bulgaria/urls.py
@@ -14,15 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
+
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
+
+from tour_guide_bulgaria import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('tour_guide_bulgaria.web.urls')),
     path('accounts/', include('tour_guide_bulgaria.accounts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] \
+              # + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 
