1ad228e3333998e96be75cdcede4ef6e8acbccba
DESKTOP-UCE6VO1\Asus-MM <yordananastasov@gmail.com>
2023-08-18 15:51:07 +0300

    Changed DEBUG=False in settings.py

--- tour_guide_bulgaria/tour_guide_bulgaria/settings.py
+++ tour_guide_bulgaria/tour_guide_bulgaria/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', 'sk')
 
 
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'False'
 APP_ENVIRONMENT = os.getenv('APP_ENVIRONMENT', 'Development')
 
 
